c00fcc033b46ab4df80c2edf497742fde1fce46b
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2020-08-13 23:14:08 +0530

    d

--- js.js
+++ js.js
@@ -1,1 +1,19 @@
-alert();
+function preview(){
+    var freader = new FileReader();
+    console.log(input);
+    // freader.readAsDataURL(input.files[0]);
+    
+    // freader.onloadend = function(event){
+    //         prdiv.innerHTML="hello";
+            
+    //     }
+}
+
+function upload(){
+    var input = document.getElementById("up-img");
+    input.click();
+
+    var prdiv = document.getElementById("preview-div");
+    prdiv.innerHTML="";
+    preview();
+}
